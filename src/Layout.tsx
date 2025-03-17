35e55eefc866f48276b24a3b148061f8861cef5a
Greg <gregminezzi@gmail.com>
2025-03-17 14:25:47 +0100

    add analytics

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,12 @@
 
 import { Outlet } from "react-router";
 import NavBar from "./NavBar";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Layout() {
   return (
     <div className="bg-green-200 flex flex-col min-h-screen">
+        <Analytics />
         <aside className="flex">
           <NavBar />
         </aside>
